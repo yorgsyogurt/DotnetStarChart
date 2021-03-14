f295725a04e3e3e7eac643e9495738e46a9421c0
Aldiyar <aldiyar.gabitov@gmail.com>
2021-03-15 00:29:50 +0600

    Initial commit

---
+++ StarChart/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
+
+namespace StarChart.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
